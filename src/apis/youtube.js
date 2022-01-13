009726e6e8e711eb0a1450116d4cd9dae599bfe8
Chrispolanco <chrispolanco@sbcglobal.net>
2022-01-12 16:02:24 -0800

    connecting axios into components

--- src/apis/youtube.js
+++ src/apis/youtube.js
@@ -7,7 +7,8 @@ export default axios.create({
     baseURL: 'https://www.googleapis.com/youtube/v3', 
     params: {
         part: 'snippet', 
-        maxResults: 5, 
+        maxResults: 5,
+        type: 'video',  
         key: KEY
     }
 })
