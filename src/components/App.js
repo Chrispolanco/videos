4086f260447fe6922eb724ea715ffb16d807d573
Chrispolanco <chrispolanco@sbcglobal.net>
2022-01-14 15:10:15 -0800

    new component created

--- src/components/App.js
+++ src/components/App.js
@@ -17,7 +17,7 @@ class App extends React.Component{
     }; 
 
     onVideoSelect = (video) => {
-        console.log("from the app!", video); 
+        this.setState({selectedVideo: video})
     }
 
     render(){
