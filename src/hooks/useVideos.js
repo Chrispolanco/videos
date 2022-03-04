8f0b4b2a67861bda4cc56a6f8da5b6f65171e8ef
Chrispolanco <chrispolanco@sbcglobal.net>
2022-03-04 10:47:04 -0800

    custom hook implements in App

---
+++ src/hooks/useVideos.js
@@ -0,0 +1,24 @@
+import { useState, useEffect } from 'react'; 
+import youtube from '../apis/youtube'; 
+
+const useVideos = (defaultSearchTerm) => {
+    const [videos, setVidoes] = useState([]); 
+
+    useEffect(() => {
+        search(defaultSearchTerm); 
+    }, []); 
+
+    const search = async term =>{
+        const response = await youtube.get('/search', {
+            params: {
+                q: term
+            }
+        }); 
+
+        setVidoes(response.data.items); 
+    }; 
+
+    return [ videos, search ]; 
+}; 
+
+export default useVideos; 
